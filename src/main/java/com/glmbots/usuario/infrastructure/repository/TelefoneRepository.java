195354b8950e20ed998459a51c2501f88487bc5b
giancarloMarquetti <gianmarquetti@gmail.com>
2025-04-08 15:13:26 -0300

    adicionadas classes de secutiry

---
+++ src/main/java/com/glmbots/usuario/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,9 @@
+package com.glmbots.usuario.infrastructure.repository;
+
+import com.glmbots.usuario.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+}
