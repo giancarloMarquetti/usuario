1c0432442e644cf3237f57bbdb452b9ceb30391e
giancarloMarquetti <gianmarquetti@gmail.com>
2025-04-09 17:06:30 -0300

    adicionado metodo que salva dados do usuario

---
+++ src/main/java/com/glmbots/usuario/business/dto/TelefoneDTO.java
@@ -0,0 +1,16 @@
+package com.glmbots.usuario.business.dto;
+
+import jakarta.persistence.Column;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TelefoneDTO {
+
+    private String ddd;
+    private String numero;
+
+}
